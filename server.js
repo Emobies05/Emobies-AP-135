ef533b979661d2a899033ceb0d72a53f2359fa22
Emobies <248824152+Emobies05@users.noreply.github.com>
2026-01-24 06:49:12 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-GitHub server.js edit:
-app.listen(process.env.PORT || 8080, ()=>console.log('Emobies live on', process.env.PORT));
+app.listen(process.env.PORT || 8080, ()=>console.log('Emobies live on', process.env.PORT)
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 8080;
 
-Procfile create:
-web: node server.js
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => res.json({status: 'OK'}));
+
+app.listen(port, () => console.log('Emobies live on', port));
