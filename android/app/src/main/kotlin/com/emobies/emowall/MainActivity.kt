a078c19b597e545494fffcfbe2998318c3d8cd62
Emobies <248824152+Emobies05@users.noreply.github.com>
2026-02-24 23:54:06 +0000

    Fix: Finalizing Android configuration

--- android/app/src/main/kotlin/com/emobies/emowall/MainActivity.kt
+++ android/app/src/main/kotlin/com/emobies/emowall/MainActivity.kt
@@ -1,3 +1,5 @@
 package com.emobies.emowall
-import io.flutter.embedding.android.FlutterFragmentActivity
-class MainActivity: FlutterFragmentActivity()
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
