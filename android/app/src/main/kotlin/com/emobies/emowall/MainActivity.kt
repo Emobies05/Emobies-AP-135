16436d465cf169018fbedd2027b0add22d503939
Emobies <248824152+Emobies05@users.noreply.github.com>
2026-02-24 22:40:44 +0000

    Fix: Kill V1 embedding

--- android/app/src/main/kotlin/com/emobies/emowall/MainActivity.kt
+++ android/app/src/main/kotlin/com/emobies/emowall/MainActivity.kt
@@ -1,5 +1,3 @@
 package com.emobies.emowall
-
 import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity()
+class MainActivity: FlutterFragmentActivity()
