e21b672e28473e8b97730abff2cec7e45c740fc1
Emobies05 <248824152+Emobies05@users.noreply.github.com>
2026-03-02 10:09:58 +0400

    fix PresignedUrl and IngestResponse

--- lib/ingest_service.dart
+++ lib/ingest_service.dart
@@ -1,18 +1,24 @@
 class PresignedUrl {
   final String uploadUrl;
-  PresignedUrl({required this.uploadUrl});
+  final String accessUrl;
+  PresignedUrl({required this.uploadUrl, required this.accessUrl});
+}
+
+class IngestResponse {
+  final int statusCode;
+  IngestResponse({required this.statusCode});
 }
 
 class IngestService {
   Future<PresignedUrl> requestPresignedUrl() async {
-    return PresignedUrl(uploadUrl: '');
+    return PresignedUrl(uploadUrl: '', accessUrl: '');
   }
 
   Future<bool> uploadToPresigned(String url, dynamic bytes) async {
     return true;
   }
 
-  Future<Map<String, dynamic>> sendIngest(dynamic payload) async {
-    return {};
+  Future<IngestResponse> sendIngest(dynamic payload) async {
+    return IngestResponse(statusCode: 200);
   }
 }
