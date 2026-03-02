bbb829911dc0df35e98ffe971b437066287d0434
Emobies05 <248824152+Emobies05@users.noreply.github.com>
2026-03-02 10:03:29 +0400

    fix IngestService methods

--- lib/ingest_service.dart
+++ lib/ingest_service.dart
@@ -1,3 +1,18 @@
+class PresignedUrl {
+  final String uploadUrl;
+  PresignedUrl({required this.uploadUrl});
+}
+
 class IngestService {
-  Future<void> ingest(String data) async {}
+  Future<PresignedUrl> requestPresignedUrl() async {
+    return PresignedUrl(uploadUrl: '');
+  }
+
+  Future<bool> uploadToPresigned(String url, dynamic bytes) async {
+    return true;
+  }
+
+  Future<Map<String, dynamic>> sendIngest(dynamic payload) async {
+    return {};
+  }
 }
