0515d91201b021099b417d6b8e8c6df17c4647c9
Emobies05 <248824152+Emobies05@users.noreply.github.com>
2026-03-06 06:04:39 +0400

    change package name to com.emobies.emowall

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.nxtbit.emobies_24"
+    namespace = "com.emobies.emowall"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.nxtbit.emobies_24"
+        applicationId = "com.emobies.emowall"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
