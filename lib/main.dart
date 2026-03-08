2150c47376f54a920df502e1d664dd834a6de1af
Emobies & TheWall = Emowall 2.0 <meradivin@gmail.com>
2026-03-08 15:04:02 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'baby/digital_amma.dart';
 import 'child/child_doctor_ai.dart';
 import 'care/guardian_ai.dart';
-import 'Health/womens_health_ai.dart';
+import 'health/women_ai.dart';
 import 'widgets/butterfly_logo.dart';
 import 'screens/media_verifier_ai.dart';
 
